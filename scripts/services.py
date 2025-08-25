70c18a51e0ff46a576a852473bbeb797921618db
mouhsen.ibrahim <mouhsen.ibrahim@gmail.com>
2025-08-25 20:56:40 +0200

    Remove debug print statement

--- scripts/services.py
+++ scripts/services.py
@@ -118,7 +118,6 @@ def main():
         if token is None:
             raise ValueError("GITHUB_TOKEN environment variable is not set")
         last_green_commit = get_last_green_commit(args.owner, args.repo, args.branch, token, args.workflow)
-        print(last_green_commit)
         print(compare_services(last_green_commit))
 
 if __name__ == '__main__':
