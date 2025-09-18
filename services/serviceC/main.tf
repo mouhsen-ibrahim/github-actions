78e8480c55cfa1075d3234dc8507e857bfb3eb91
mouhsen.ibrahim <mouhsen.ibrahim@gmail.com>
2025-09-18 18:21:07 +0200

    feat: enable compute api

--- services/serviceC/main.tf
+++ services/serviceC/main.tf
@@ -23,6 +23,15 @@ provider "google" {
   project = var.project_id
 }
 
+resource "google_project_service" "services" {
+  for_each = toset([
+    "compute.googleapis.com",
+  ])
+  project            = var.project_id
+  service            = each.key
+  disable_on_destroy = false
+}
+
 resource "random_string" "sample" {
   length  = 30
   upper   = true
