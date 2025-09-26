58b63ea4c6762cbd5ff6590ce4f3bab3fb9f6a19
mouhsen.ibrahim <mouhsen.ibrahim@gmail.com>
2025-09-26 23:21:23 +0200

    remove google provider

--- services/serviceC/main.tf
+++ services/serviceC/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = "1.13.1"
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "7.3.0"
-    }
-  }
   backend "azurerm" {
     use_azuread_auth     = true
     storage_account_name = "githubactionsterraform"
@@ -13,14 +7,4 @@ terraform {
     container_name       = "services"
     key                  = "serviceC"
   }
-}
-
-provider "google" {
-  project = var.project_id
-}
-
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
-  default = "github-actions-472416"
 }
